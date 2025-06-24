e3c65cd4d980e8a831f863fda71a993c6a23ec6f
unknown <johnnylyon231@gmail.com>
2025-06-24 13:00:00 -0700

    url solved

--- client/src/utility/constant.ts
+++ client/src/utility/constant.ts
@@ -1,2 +1,5 @@
-export const baseUrl = "http://localhost:3000/api";
+export const baseUrl =
+  process.env.NODE_ENV === "developement"
+    ? "http://localhost:3000/api"
+    : "https://taskly-task-management-app.onrender.com/api";
 // export const baseUrl = "http:/Taskly-task-management-app/api"
